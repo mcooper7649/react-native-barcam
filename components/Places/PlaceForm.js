696abe5c5c438a0a8dd755da997fa552217b655b
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2023-02-03 08:55:47 -0500

    added Colors Pallette, configured add place logic for navigator. Configured PlaceForm Component to handle user input

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,11 +1,43 @@
-import { View } from 'react-native';
-
+import { Text, ScrollView, TextInput, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { Colors } from '../../constants/Colors';
 function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState('');
+
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <Text style={styles.label}>Title</Text>
+      <TextInput
+        style={styles.input}
+        value={enteredTitle}
+        onChangeText={changeTitleHandler}
+      />
+    </ScrollView>
   );
 }
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    color: Colors.primary700,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomWidth: 2,
+    borderBottomColor: Colors.primary700,
+    backgroundColor: Colors.primary100,
+  },
+});
